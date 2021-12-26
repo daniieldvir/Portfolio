295f0a6f422ceb037f41a7bea5f6228b31733d0f
daniieldvir <91826695+daniieldvir@users.noreply.github.com>
2021-12-26 22:14:01 +0200

    contact

--- src/cmps/contact-me.vue
+++ src/cmps/contact-me.vue
@@ -1,9 +1,21 @@
 <template>
   <section id="contact-me" class="contact-me main-layout full">
     <h1>Contact Me</h1>
-    <p>Tel: 054-5586006</p>
-    <p>Mail: Daniieldvir2406@gmail.com</p>
-
+    <!-- <p>Tel: 054-5586006</p> -->
+    <!-- <p>Mail: Daniieldvir2406@gmail.com</p> -->
+    <section>
+      <a class="tel" href="tel:054-5586006"
+        ><img
+          src="https://res.cloudinary.com/disku3v4j/image/upload/v1640549113/daniiel/icons8-phone-30_ylnw49.png"
+        />
+        054-5586006</a
+      >
+      <a class="email" href="mailto:daniieldvir2406@gmail.com">
+        <img
+          src="https://res.cloudinary.com/disku3v4j/image/upload/v1640549113/daniiel/icons8-mail-24_rare7o.png"
+        />Click to email me</a
+      >
+    </section>
     <section class="contact-btn">
       <button>
         <a href="https://www.linkedin.com/in/daniiel-dvir-06154769/">
